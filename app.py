53564c590bbf4593dda5eef0eed069b443065b20
Chadi Abdelghani Idrissi <r0786882@student.thomasmore.be>
2023-12-14 20:29:44 +0100

    image prediction

--- app.py
+++ app.py
@@ -14,9 +14,26 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from keras.utils import image_dataset_from_directory
+from keras.preprocessing import image
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(layout="wide")
+
+uploaded_file = None
+
+# Section explaining the application
+st.title("Wild Cat Image Classifier")
+
+st.write(
+    "Welcome to the Wild Cat Image Classifier app! This application uses a deep learning model "
+    "to classify images of wild cats into different species such as lion, tiger, leopard, cheetah, and panther."
+)
+
+st.write(
+    "Whether you're a wildlife enthusiast, a researcher, or just curious about wild cats, "
+    "you can use this app to upload an image, train the model and get predictions on the species of the wild cat in the image."
+)
+
 
 # Constants
 NUM_CLASSES = 5
@@ -24,7 +41,15 @@ IMG_SIZE = 64
 HEIGHT_FACTOR = 0.1
 WIDTH_FACTOR = 0.1
 BATCH_SIZE = 32
-EPOCHS = 20
+
+
+IMG_SIZE = st.sidebar.slider('Image size', min_value=32, max_value=128, value=64,step=16)
+
+BATCH_SIZE = st.sidebar.slider('Batch size', min_value=16, max_value=64, value=32,step=16)
+
+EPOCHS = st.sidebar.slider('Number of Epochs', min_value=1, max_value=20, value=10)
+# Sidebar Configuration for Training
+initial_learning_rate = st.sidebar.number_input('Learning Rate', min_value=0.001, max_value=1.0, value=0.001, step=0.001, format="%.3f")
 
 # Set the parameters for your data
 image_size = (IMG_SIZE, IMG_SIZE)
@@ -86,7 +111,7 @@ def perform_eda(dataset_dir, num_samples_to_visualize):
         st.write(f"{cls} images count: {count}")
 
 
-def build_and_train_model(train_ds, validation_ds, num_epochs, learning_rate):
+def train_model(train_ds, validation_ds, num_epochs, learning_rate):
     # Model Definition
     model = tf.keras.Sequential([
         layers.Resizing(IMG_SIZE, IMG_SIZE),
@@ -109,31 +134,16 @@ def build_and_train_model(train_ds, validation_ds, num_epochs, learning_rate):
         layers.Dense(NUM_CLASSES, activation="softmax")
     ])
 
-    initial_learning_rate = 0.001
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
     )
     optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 
-
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-    # Create a callback to save the best model during training
-    model_checkpoint_callback = ModelCheckpoint(
-        filepath='wild_cat.h5',
-        save_best_only=True,
-        monitor='val_loss',
-        mode='min',
-        verbose=1
-    )
-
-    # Compile and train your model as usual
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
     history = model.fit(train_ds,
                             validation_data=validation_ds,
                             epochs=num_epochs,
-                            callbacks=[model_checkpoint_callback],
                             verbose=1)
     return history, model
 
@@ -147,9 +157,6 @@ classes = os.listdir(dataset_dir)
 # Perform EDA
 perform_eda(dataset_dir, num_samples_to_visualize=5)
 
-# Sidebar Configuration for Training
-num_epochs = st.sidebar.slider('Number of Epochs', min_value=1, max_value=30, value=10)
-learning_rate = st.sidebar.number_input('Learning Rate', min_value=0.001, max_value=1.0, value=0.001, step=0.001, format="%.3f")
 
 # Load Datasets
 train_ds = image_dataset_from_directory(
@@ -182,10 +189,13 @@ test_ds = image_dataset_from_directory(
         image_size=image_size
 )
 
+# Upload image through Streamlit
+uploaded_file = st.sidebar.file_uploader("Upload an image to predict (optional)", type=["jpg", "jpeg", "png"])
+
 # Model Training
 if st.sidebar.button('Start Training'):
     with st.spinner('Training in progress...'):
-        history, model = build_and_train_model(train_ds, validation_ds, num_epochs, learning_rate)
+        history, model = train_model(train_ds, validation_ds, EPOCHS, initial_learning_rate)
     st.success('Training completed.')  # Display a success message when training is complete
     
     st.subheader('Loss and Accuracy curves')
@@ -231,3 +241,26 @@ if st.sidebar.button('Start Training'):
     display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=class_labels)
     display.plot(cmap='viridis', values_format='d', ax=ax)
     st.pyplot(fig)
+
+    if uploaded_file is not None:
+        # Display the uploaded image
+        st.image(uploaded_file, caption="Uploaded Wild Cat Image", use_column_width=False)
+
+        # Preprocess the uploaded image for the model
+        img = image.load_img(uploaded_file, target_size=(64, 64))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Make prediction
+        with st.spinner("Making Prediction..."):
+            result = model.predict(img_array)
+            prediction = np.argmax(result)
+
+        # Map the prediction index to the actual class name using class_labels
+        predicted_class = class_labels[prediction]
+        
+        confidence_percentage = result[0][prediction] * 100
+
+        # Display the prediction result
+        st.subheader("Prediction Result:")
+        st.write(f"The model predicts that the image contains a {predicted_class} with {confidence_percentage:.2f}% confidence.")
